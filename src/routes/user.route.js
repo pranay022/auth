d1a39c3a7fb1ec6a1d9012c02d2d3e4feb26c67a
pranay <142023993+pranay022@users.noreply.github.com>
2024-11-16 16:42:55 +0530

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,11 +5,8 @@ const userAuth = require('../middleware/userAuth')
 
 const router = express.Router()
 
-//signup endpoint
-//passing the middleware function to the signup
 router.post('/signup', userAuth.saveUser, signup)
 
-//login route
 router.post('/login', login )
 
 module.exports = router
